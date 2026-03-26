075151b5dfa83ec2bd8580ba8a41724839273095
LaloMqzR <eduardo.marquez@upaep.mx>
2026-03-26 08:38:01 -0600

    Create Inventario.cs

---
+++ Inventario.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class Inventario 
+{
+    public int ContadorId {get; set;}
+
+    //List<Producto>productos=new List<Producto>();
+    
+    public void AgregarProducto()
+    {
+        
+    }
+    
+    public void ListarProductos()
+    {
+        
+    }
+    
+    public void BuscarProducto()
+    {
+        
+    }
+    
+    public void ActualizarStock()
+    {
+        
+    }
+    
+    public void EliminarProducto()
+    {
+        
+    }
+}
+
+public class Producto
+{
+    public string Nombre {get; set; }
+    public int Id {get; set; } 
+    public int Stock {get; set;}
+}
+
+public class Program 
+{
+    static void Main()
+    {
+        
+    }
+}
